51c547b65a198b9db141d1753a45794f7097a58e
Hoà Võ <vothaihoa19102003@gmail.com>
2024-03-23 14:48:23 +0700

    update

--- discovery-server/src/main/java/com/devhoa/discoveryserver/config/SecurityConfig.java
+++ discovery-server/src/main/java/com/devhoa/discoveryserver/config/SecurityConfig.java
@@ -1,17 +1,13 @@
 package com.devhoa.discoveryserver.config;
 
-import org.glassfish.hk2.api.Customize;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -27,23 +23,25 @@ public class SecurityConfig {
 
     @Bean
     public UserDetailsService userDetailsService() {
-        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        UserDetails user = User
-                .withUsername(username)
-                .password(encoder.encode(password))
-                .roles("USER")
+        User.UserBuilder users = User.withDefaultPasswordEncoder();
+        UserDetails user = users
+                .username(username)
+                .password(password)
+                .authorities("USER")
                 .build();
         return new InMemoryUserDetailsManager(user);
     }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf->csrf.ignoringRequestMatchers("/eureka/**"))
-                .authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated())
-                .formLogin(Customizer.withDefaults());
+        http.csrf().disable()
+                .authorizeHttpRequests(
+                        authorize -> authorize
+                        .anyRequest()
+                        .authenticated())
+                .httpBasic();
         return http.build();
     }
-
 
 
 }
